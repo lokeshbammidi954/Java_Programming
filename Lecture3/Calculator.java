0152f5b81452ebf53349dcb2f1f118025341a2cd
lokeshbammidi954 <lokeshbammidi954@outlook.com>
2024-12-22 09:51:34 +0530

    Added the lecture 3 it contains the conditional statements with practice questions

--- Lecture3/Calculator.java
+++ Lecture3/Calculator.java
@@ -1,4 +1,11 @@
-
+/*Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows : 
+	1 : + (Addition) a + b
+2 : - (Subtraction) a - b
+3 : * (Multiplication) a * b
+4 : / (Division) a / b
+5 : % (Modulo or remainder) a % b
+Calculate the result according to the operation given and display it to the user.
+ */
 package Java_Programming.Lecture3;
 import java.util.*;
 public class Calculator {
