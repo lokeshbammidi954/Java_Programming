6172032456581b4d143ba128e82d978f79504ee1
lokeshbammidi954 <lokeshbammidi954@outlook.com>
2024-12-22 21:19:55 +0530

    added 4 other patterns

---
+++ LecturePatterns/PalindromeTriangle.java
@@ -0,0 +1,21 @@
+package Java_Programming.LecturePatterns;
+import java.util.*;
+public class PalindromeTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The N Value : ");
+        int N =sc.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2; j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LecturePatterns/ZeroOneTriangle.java
@@ -0,0 +1,20 @@
+package Java_Programming.LecturePatterns;
+import java.util.*;
+public class ZeroOneTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The N Value : ");
+        int N =sc.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                if ((i+j)%2 == 0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
