d4eccabe52850156cad3135b63ec539a62c11fd7
lokeshbammidi954 <lokeshbammidi954@outlook.com>
2025-01-16 10:23:52 +0530

    Modified

--- Recursion/FactorialOFN.java
+++ Recursion/FactorialOFN.java
@@ -5,11 +5,11 @@ public class FactorialOFN {
         if(n == 0 || n==1 ) {
             System.out.println(fact);
             return;
-            }
-            fact *= n;
-            Fact(n-1, fact);
-            }
-            public static void main(String args[]) {
-            Fact(5, 1);
+        }
+        fact *= n;
+        Fact(n-1, fact);
+    }
+    public static void main(String args[]) {
+        Fact(5, 1);
     }
 }
--- Recursion/Printreverse.java
+++ Recursion/Printreverse.java
@@ -3,10 +3,10 @@ package Java_Programming.Recursion;
 public class Printreverse {
     public static void PrintRev(String S,int n){
         if(n==0){
-            System.out.println(S.charAt(n));
+            System.out.print(S.charAt(n));
             return;
         }
-        System.out.println(S.charAt(n));
+        System.out.print(S.charAt(n));
         PrintRev(S, n-1);
     }
     public static void main(String[] args) {
